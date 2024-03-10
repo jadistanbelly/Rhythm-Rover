9fc13e8449efaecbb18e6f505e98c4b821aa59c2
jadistanbelly <jadelkareem@hotmail.com>
2024-03-09 19:14:58 -0500

    Added queue system

--- ringmaster_riff/bot.py
+++ ringmaster_riff/bot.py
@@ -1,5 +1,6 @@
 import discord
 import asyncio
+from collections import deque
 
 # Initialize the bot with intents (required for certain events)
 intents = discord.Intents.default()
@@ -9,13 +10,17 @@ client = discord.Client(intents=intents)
 # Dictionary to map user IDs to audio file paths
 user_audio_files = {
     "426031900633858048": "C:\\Users\\jadis\\OneDrive\\discord_bots\\ringmaster_riff\\audio\\johncena.mp3", # Jad
-    "169183728277389312": "C:\\Users\\jadis\\OneDrive\\discord_bots\\ringmaster_riff\\audio\\undertaker.mp3", # Gogo
-    "169178004684144650": "C:\\Users\\jadis\\OneDrive\\discord_bots\\ringmaster_riff\\audio\\reymysterio.mp3", # Hamood
+    "169183728277389312": "C:\\Users\\jadis\OneDrive\\discord_bots\\ringmaster_riff\\audio\\therock.mp3", # Gogo
+    "169178004684144650": "C:\\Users\\jadis\\OneDrive\\discord_bots\\ringmaster_riff\\audio\\undertaker.mp3", # Hamood
     "304642517343928321": "C:\\Users\\jadis\\OneDrive\\discord_bots\\ringmaster_riff\\audio\\tripleh.mp3", # Ahmad
     "169793619908231168": "C:\\Users\\jadis\\OneDrive\\discord_bots\\ringmaster_riff\\audio\\randyorton.mp3", # Scott
-    "641995250067570719": "C:\\Users\\jadis\\OneDrive\\discord_bots\\ringmaster_riff\\audio\\batista.mp3" # Farris
+    "641995250067570719": "C:\\Users\\jadis\\OneDrive\\discord_bots\\ringmaster_riff\\audio\\batista.mp3", # Farris
+    "671778028178898944": "C:\\Users\\jadis\\OneDrive\\discord_bots\\ringmaster_riff\\audio\\reymysterio.mp3" # Ben
     # Add more user IDs and corresponding audio files as needed
 }
+
+# Queue to manage audio playback order
+audio_queue = deque(maxlen=3)  # Set the queue limit to 10
 
 @client.event
 async def on_ready():
@@ -28,12 +33,21 @@ async def on_voice_state_update(member, before, after):
             user_id = str(member.id)
             if user_id in user_audio_files:
                 audio_file_path = user_audio_files[user_id]
-                vc = await after.channel.connect()
-                vc.play(discord.FFmpegOpusAudio(executable= "C:\\ffmpeg\\tools\\ffmpeg\\bin\\ffmpeg.exe", source = audio_file_path))
-                #vc.play(discord.FFmpegPCMAudio(executable= "C:\\ffmpeg\\tools\\ffmpeg\\bin\\ffmpeg.exe",source = audio_file_path))
-                while vc.is_playing():
-                    await asyncio.sleep(1)
-                await vc.disconnect()
+                audio_queue.append(audio_file_path)  # Add to the queue
+
+                # If the queue is not empty and the bot is not currently playing
+                if audio_queue and not client.voice_clients:
+                    await play_next_audio(after.channel)
+
+async def play_next_audio(channel):
+    while audio_queue:
+        audio_file_path = audio_queue.popleft()
+        vc = await channel.connect()
+        vc.play(discord.FFmpegPCMAudio(executable="C:\\ffmpeg\\tools\\ffmpeg\\bin\\ffmpeg.exe", source=audio_file_path))
+        while vc.is_playing():
+            await asyncio.sleep(1)
+        await vc.disconnect()
+
 
 # Run the bot
 client.run("MTIxNTgzMTU2NTEwMDM4ODM4Mg.GhwKLu.Q_zzzlPZ7KbkwoYU-XMVf7-8avbMJTqeyNyK10")
