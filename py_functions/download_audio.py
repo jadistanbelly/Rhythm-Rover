331eece29821e6d05a1990f274009cc03fde6a16
jadistanbelly <jadelkareem@hotmail.com>
2024-04-19 19:23:56 -0400

    Shortened Code Closes #13 added pycache for download_audio to gitignore

--- py_functions/download_audio.py
+++ py_functions/download_audio.py
@@ -1,51 +1,26 @@
 import yt_dlp
 from yt_dlp.utils import download_range_func
-import subprocess
 import re
-import os
 
 # Function to download audio from url using yt_dlp
 def download_audio(url, output_path, start, end):
+    # Split youtube video from playlist
+    url = url.split("&list=")[0] if "&list=" in url else url
 
-    # Split video link from playlist
-    if "&list=" in url:
-        split = url.split("&list=")
-        url = split[0]
-
-    # Get title of download 
-    with yt_dlp.YoutubeDL() as ydl:
-        info_dict = ydl.extract_info(url, download=False) # dont download url yet
-        base_title = info_dict['title'] # Extract title 
-        clean_title = re.sub(r'[^\w\s]', '', base_title) # Clean title of any special characters 
-        print(clean_title)
-        video_title = f'{clean_title}{str(start)}{str(end)}' # create unique title name
-        print(video_title)
+    with yt_dlp.YoutubeDL() as ydl: # Get title of download 
+        info_dict = ydl.extract_info(url, download=False) # False, so we can extract title first
+        clean_title = re.sub(r'[^\w\s]', '_', info_dict['title']) # Extract and clean title of any special characters 
+        video_title = f'{clean_title}_{str(start)}_{str(end)}' # create unique title name
 
     # Setup options for ydl
     ydl_opts = {
-        'extract_audio': True,
-        'format': 'bestaudio',
-        'outtmpl': f'{output_path}{video_title}.mp3', # Title audio file as per video title from url including timestamps
+        'outtmpl': f'{output_path}{video_title}.%(ext)s', # Title audio file as per video title from url including timestamps
         'download_ranges': download_range_func(None, [(start,end)]), # Specified timestamps in int format
         'force_keyframes_at_cuts': True,
-        #'noplaylist': True # Code not working
-    }
-    try: # Try just audio
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-            return video_title
+        'postprocessors': [{'key': 'FFmpegExtractAudio','preferredcodec': 'mp3','preferredquality': '192'}], # Use FFMPEG to extract audio from video
+        'quiet': True} # Remove quiet if you would like to see the progress of the download in console
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url]) # Download audio from url         
         
-    except: # fallback to video format
-        ydl_opts['format'] = 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best'
-        ydl_opts['outtmpl'] = f'{output_path}{video_title}.mp4'
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-
-            # Convert to audio
-            subprocess.run(["ffmpeg", "-i", f'{output_path}{video_title}.mp4', "-vn", "-acodec", "libmp3lame", "-y", f'{output_path}{video_title}.mp3'])
-
-            # Delete Video
-            os.remove(f'{output_path}{video_title}.mp4')
-
-            # Return audio title
-            return video_title
+    return video_title
