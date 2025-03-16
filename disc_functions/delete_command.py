925b8f88720375119f2e8b70ce85fd71ca2e5a95
jadistanbelly <jadelkareem@hotmail.com>
2025-03-16 15:41:41 -0400

    refactor delete command to improve error handling and cleanup logic

--- disc_functions/delete_command.py
+++ disc_functions/delete_command.py
@@ -1,7 +1,8 @@
-from variables import tree, user_audio_files # Change from variables to configs to run on your own bot
+from variables import tree, user_audio_files
 import discord
 import os
 from py_functions.sync_db import sync_db
+
 @tree.command(
     name="delete",
     description="Use only if you want to be permanently deleted",
@@ -9,13 +10,24 @@ from py_functions.sync_db import sync_db
 )
 async def delete(interaction: discord.Interaction):
     '''Command to delete user intro and/or outro'''
-    user_id = str(interaction.user.id) # Get user ID
-    intro_path, outro_path = user_audio_files[user_id] # Get intro and outro path
     try:
-        if os.path.exists(intro_path): os.remove(intro_path) # Remove intro
-        if os.path.exists(outro_path): os.remove(outro_path) # Remove outro
-        del user_audio_files[user_id] # Remove user from keys
-    except KeyError or FileNotFoundError:
-        await interaction.response.send_message(f'You have no audio files to delete', ephemeral = True)
-    sync_db()
-    await interaction.response.send_message(f'Deleted!', ephemeral = True) # Tell user that operation was successful
+        user_id = str(interaction.user.id)
+        intro_path, outro_path = user_audio_files[user_id]
+
+        if os.path.exists(intro_path): os.remove(intro_path)
+        if os.path.exists(outro_path): os.remove(outro_path)
+        del user_audio_files[user_id]
+
+        sync_db()
+        await interaction.response.send_message(f'Deleted!', ephemeral=True)
+
+    except KeyError:
+        await interaction.response.send_message(f'You have no audio files to delete', ephemeral=True)
+        
+    except (TypeError, FileNotFoundError):
+        if user_id in user_audio_files:
+            del user_audio_files[user_id]
+            sync_db()
+            await interaction.response.send_message(f'Partially deleted audio files and cleaned up data', ephemeral=True)
+        else:
+            await interaction.response.send_message(f'No data found to delete', ephemeral=True)
